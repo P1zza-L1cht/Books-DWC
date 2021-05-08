825f9258cb3bd798acb6de902209b43030d67bf8
P1zza-L1cht <ec2-user@ip-172-31-47-58.ec2.internal>
2021-05-08 04:56:12 +0000

    [add]indexの内容追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,26 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path(book.id)
   end
 
   def edit
+  end
+
+  def book_params
+    params.permit(:title, :body)
   end
 end
