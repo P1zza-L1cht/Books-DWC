1460cf256938455259ea36ababddbebfb6c68162
P1zza-L1cht <ec2-user@ip-172-31-47-58.ec2.internal>
2021-05-08 06:11:43 +0000

    [add]showページ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,10 +14,17 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path(book.id)
+    redirect_to "/books/#{book.id}"
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to "/books/#{book.id}"
   end
 
   def book_params
