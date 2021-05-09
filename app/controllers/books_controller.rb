071e0888d772eadf8124e6875e25851fec8bb9cb
P1zza-L1cht <ec2-user@ip-172-31-47-58.ec2.internal>
2021-05-09 03:17:53 +0000

    [add]destroy追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,12 @@ class BooksController < ApplicationController
     redirect_to "/books/#{book.id}"
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
